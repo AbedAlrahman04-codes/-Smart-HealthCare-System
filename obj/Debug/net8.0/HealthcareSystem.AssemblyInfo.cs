51f2c96a353e6eab1cbf894aa6967633e1cc347c
Abedalrahman Assfour <aboodasfoor384@gmail.com>
2026-03-31 22:53:56 +0300

    remove bin and obj

--- obj/Debug/net8.0/HealthcareSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/HealthcareSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70f3d8d471ec7150cfa03de3cebc3272d1c02e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
